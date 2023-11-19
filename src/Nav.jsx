c3ed36991d7640c0455fefd9351761554edb5123
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-11-19 21:33:04 +0400

    c

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Nav() {
   return (
     <div className="nav-main">
+      <img className="logo" src="../img/box-logo.png" alt="logo" />
       <ul className="nav-list">
-        <img src="" alt="" />
         <li className="nav-list-item">home</li>
         <li className="nav-list-item">about</li>
         <li className="nav-list-item">top 10</li>
