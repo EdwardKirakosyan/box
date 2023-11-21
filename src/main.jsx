b6a2b9a217bde0c6077c38afdd465251d132345a
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-11-21 16:46:29 +0400

    c

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,10 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+
+const app = require("express")()
+app.get("/", (req, res) => res.json({ message: "DOckER!!!" }))
+const port = process.env.PORT || 8080
+app.listen(port, () =>
+  console.log(`app listenning on http://localhost:${port}`)
+)
