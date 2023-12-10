f4acb63cfefa6e4c5bfc800635cd8dffe1f5a0b5
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-12-10 17:24:08 +0400

    c

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,13 @@ export default function Nav() {
   return (
     <>
       <div className="nav-main">
-        <p>About page</p>
+        <h2>About page</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, eos
+          molestias optio nihil, at, deleniti animi natus aspernatur ad facere
+          ullam quibusdam atque temporibus a minus nostrum nisi voluptatibus
+          qui!
+        </p>
       </div>
     </>
   )
